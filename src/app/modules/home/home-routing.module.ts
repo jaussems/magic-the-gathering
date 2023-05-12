3c695b63bc4ddea166e1f7219fc06d5fb5ca0237
jaussems <jannesaussems@gmail.com>
2023-05-12 20:08:54 +0200

    reconfigured module import in routing, added card-page component

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { CardPageComponent } from './card-page/card-page.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'card', component: CardPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,17 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { CardComponent } from './card/card.component';
-
+import { CardPageComponent } from './card-page/card-page.component';
 
 @NgModule({
-  declarations: [
-    HomeComponent,
-    CardComponent
-  ],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  declarations: [HomeComponent, CardPageComponent],
+  imports: [CommonModule, HomeRoutingModule],
 })
-export class HomeModule { }
+export class HomeModule {}
