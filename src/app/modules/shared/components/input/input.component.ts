be89419ad87e9075dbe1a39fdfc81cd476fe2e4e
jaussems <jannesaussems@gmail.com>
2023-05-14 20:18:23 +0200

    added formControl

--- src/app/modules/shared/components/input/input.component.ts
+++ src/app/modules/shared/components/input/input.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
@@ -9,4 +10,5 @@ export class InputComponent {
   @Input() label?: string;
   @Input() placeholder?: string;
   @Input() isFocused?: boolean;
+  @Input() control!: FormControl;
 }
