5d5565679982044c34a868fc2bf3b19a98f9866a
jaussems <jannesaussems@gmail.com>
2023-05-14 20:16:55 +0200

    added input component

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -4,9 +4,10 @@ import { CardComponent } from './components/card/card.component';
 import { SpinnerComponent } from './components/spinner/spinner.component';
 import { InputSelectComponent } from './components/input-select/input-select.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { InputComponent } from './components/input/input.component';
 
 @NgModule({
-  declarations: [CardComponent, SpinnerComponent, InputSelectComponent],
+  declarations: [CardComponent, SpinnerComponent, InputSelectComponent, InputComponent],
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
   exports: [CardComponent, SpinnerComponent, InputSelectComponent],
 })
